2984a4ea5b5ba1121c1dcd231dc7749aab3b2deb
Kushal Banda <kushalbanda265@gmail.com>
2025-11-23 13:28:21 +0530

    Add subgraph functionality and visualizations
    - Introduced a new Python module `Subgraphs.py` for managing subgraphs, including state transformations and node definitions.
    - Added two new binary image files, `parent_graph.png` and `subgraph.png`, for visual representation of the graphs.
    - Implemented a mechanism to generate visualizations of the parent and subgraph structures, enhancing the project's graphical capabilities.

---
+++ Langgraph/Subgraphs/Subgraphs.py
@@ -0,0 +1,39 @@
+from typing_extensions import TypedDict
+from langgraph.graph.state import StateGraph, START
+from IPython.display import Image, display
+
+class SubgraphState(TypedDict):
+    bar: str
+
+# Subgraph
+
+def subgraph_node_1(state: SubgraphState):
+    return {"bar": "hi! " + state["bar"]}
+
+subgraph_builder = StateGraph(SubgraphState)
+subgraph_builder.add_node(subgraph_node_1)
+subgraph_builder.add_edge(START, "subgraph_node_1")
+subgraph = subgraph_builder.compile()
+
+# Parent graph
+
+class State(TypedDict):
+    foo: str
+
+def call_subgraph(state: State):
+    # Transform the state to the subgraph state
+    subgraph_output = subgraph.invoke({"bar": state["foo"]})  
+    # Transform response back to the parent state
+    return {"foo": subgraph_output["bar"]}
+
+builder = StateGraph(State)
+builder.add_node("node_1", call_subgraph)
+builder.add_edge(START, "node_1")
+graph = builder.compile()
+
+
+print(graph.invoke({"foo": "Hello, World!"}))
+
+# Generate visualizations
+parent_graph_png = graph.get_graph().draw_mermaid_png()
+subgraph_png = subgraph.get_graph().draw_mermaid_png()
