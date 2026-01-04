a379d350bceaa7af8042da8fdea16630b2f77ab2
Kushal Banda <kushalbanda265@gmail.com>
2026-01-04 22:12:26 +0530

    feat: add cache connection logic

---
+++ Redis/vectorLibrary/src/cache/connection.py
@@ -0,0 +1,10 @@
+from redis import Redis
+from config.settings import Settings
+
+class Connection:
+    def __init__(self):
+        self.settings = Settings()
+        self.redis_client = Redis(url=self.settings.get_redis_url())
+
+    def ping(self):
+        return self.redis_client.ping()
