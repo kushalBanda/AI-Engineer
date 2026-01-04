9e4e7dba4d97c7177f1e335c3dbdffbd8bf2f546
Kushal Banda <kushalbanda265@gmail.com>
2026-01-04 22:12:15 +0530

    feat: add main entry point

---
+++ Redis/vectorLibrary/src/main.py
@@ -0,0 +1,21 @@
+import pprint
+from agents import build_graph
+
+
+def main():
+    graph = build_graph()
+    inputs = {
+        "messages": [
+            ("user", "What does Lilian Weng say about the types of agent memory?"),
+        ]
+    }
+    for output in graph.stream(inputs):
+        for key, value in output.items():
+            pprint.pprint(f"Output from node '{key}':")
+            pprint.pprint("---")
+            pprint.pprint(value, indent=2, width=80, depth=None)
+        pprint.pprint("\n---\n")
+
+
+if __name__ == "__main__":
+    main()
