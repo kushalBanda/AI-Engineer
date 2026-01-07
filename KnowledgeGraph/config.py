e8d6926580692e5e00d81d110cb5ec60991d1639
Kushal Banda <kushalbanda265@gmail.com>
2026-01-07 22:02:34 +0530

    feat: add configuration management for Neo4j and OpenAI API

---
+++ KnowledgeGraph/config.py
@@ -0,0 +1,13 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    def __init__(self):
+        self.neo4j_uri = os.getenv("NEO4J_URI")
+        self.neo4j_user = os.getenv("NEO4J_USERNAME")
+        self.neo4j_password = os.getenv("NEO4J_PASSWORD")
+        self.neo4j_database = os.getenv("NEO4J_DATABASE")
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.model = os.getenv("OPENAI_MODEL")
