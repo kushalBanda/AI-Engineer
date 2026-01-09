223a5d8061eff0b945d6b6125ea264f906985bba
Kushal Banda <kushalbanda265@gmail.com>
2026-01-09 08:28:15 +0530

    feat: add configuration and quickstart scripts for Neo4j integration with environment variable management

---
+++ KnowledgeGraph/Neo4j/config.py
@@ -0,0 +1,14 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    def __init__(self):
+        self.neo4j_uri = os.getenv("NEO4J_URI")
+        self.neo4j_username = os.getenv("NEO4J_USERNAME")
+        self.neo4j_password = os.getenv("NEO4J_PASSWORD")
+        self.neo4j_database = os.getenv("NEO4J_DATABASE")
+        self.aura_instance_id = os.getenv("AURA_INSTANCE_ID")
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.model = os.getenv("OPENAI_MODEL")
